5a37a8f1da90699121a3949e1ffe7edc881c8116
defense <swastiksrichandan3@gmail.com>
2023-01-25 11:20:22 +0530

    exit all added

--- main.py
+++ main.py
@@ -32,13 +32,25 @@ def handle_post_request():
     d = data[0]
     print(d)
 
-    # normal place order
-    if d.get('simple') == 'true':
+    # 1. normal place order
+    if d.get('simple') is True:
         norder = api.place_order(d.get('buy_or_sell'), d.get('product_type'), d.get('exchange'), d.get('tradingsymbol'),
                                  d.get('quantity'), 0, d.get('price_type'), float(d.get('price')), remarks="api place")
         print("finvasia log")
         print(norder)
-    #
+    # 3.exit all position
+    if d.get('exitall') is True:
+        netpos = api.get_positions()
+        for o in netpos:
+            if int(o['netqty']) != 0:
+                transactiontype = "S" if int(o['netqty']) > 0 else "B"
+                api.place_order(buy_or_sell=transactiontype, product_type=o['prd'],
+                                exchange=o['exch'], tradingsymbol=o['tsym'],
+                                quantity=abs(int(o['netqty'])), discloseqty=0, price_type='MKT', trigger_price=None,
+                                retention='DAY', )
+    # 2. close same instrument and place new order , if instrument starting with banknifty then close all instrument
+    # contains banknfifty if nifty then close all instrument contains nifty, if exchange is nse then close the current
+
 
     return '200'
 
